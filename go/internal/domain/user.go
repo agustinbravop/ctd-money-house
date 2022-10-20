1653207dd5d554adf480ef3485c182369a34682d
AgusBrr <anbravoperez@gmail.com>
2022-10-19 21:57:45 -0300

    estructura inical del proyecto

---
+++ go/internal/domain/user.go
@@ -0,0 +1,13 @@
+package domain
+
+type User struct {
+	ID        int
+	Name      string
+	LastName  string
+	Dni       string
+	Email     string
+	Telephone string
+	Password  string
+	Cvu       int
+	Alias     string
+}

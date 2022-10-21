39a849f8ccd5eb98e91d9d23d21112b91553696a
AgusBrr <anbravoperez@gmail.com>
2022-10-20 22:47:44 -0300

    [ADD] creado KeycloakClient que se comunica con Keycloak, con los métodos GetUserByID y LoginUser

---
+++ internal/config/keycloak.go
@@ -0,0 +1,60 @@
+package config
+
+import (
+	"context"
+	"ctd-money-house/internal/domain"
+	"github.com/Nerzal/gocloak/v12"
+)
+
+type KeycloakClient interface {
+}
+
+type keycloakClient struct {
+	jwt          *gocloak.JWT
+	client       *gocloak.GoCloak
+	ctx          context.Context
+	realm        string
+	clientID     string
+	clientSecret string
+}
+
+func NewKeycloakClient(url, clientID, clientSecret, realm string) (KeycloakClient, error) {
+	client := gocloak.NewClient(url)
+	ctx := context.Background()
+	token, err := client.LoginClient(ctx, clientID, clientSecret, realm)
+	if err != nil {
+		return keycloakClient{}, err
+	}
+
+	return keycloakClient{
+		jwt:          token,
+		client:       client,
+		clientSecret: clientSecret,
+		clientID:     clientID,
+		realm:        realm,
+		ctx:          ctx,
+	}, nil
+}
+
+// GetUserByID solicita a Keycloak los datos del usuario con el userID dado.
+func (k *keycloakClient) GetUserByID(userID string) (domain.User, error) {
+	kcUser, err := k.client.GetUserByID(k.ctx, k.jwt.AccessToken, k.realm, userID)
+	if err != nil {
+		return domain.User{}, err
+	}
+	return toDomainUser(kcUser), nil
+}
+
+// LoginUser realiza el inicio de sesión en Keycloak y retorna el JWT de la sesión.
+func (k *keycloakClient) LoginUser(email, password string) (*gocloak.JWT, error) {
+	return k.client.Login(k.ctx, k.clientID, k.clientSecret, k.realm, email, password)
+}
+
+func toDomainUser(kcUser *gocloak.User) domain.User {
+	return domain.User{
+		Name:       *kcUser.FirstName,
+		LastName:   *kcUser.LastName,
+		Email:      *kcUser.Email,
+		KeycloakID: *kcUser.ID,
+	}
+}

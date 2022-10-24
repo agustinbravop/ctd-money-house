e8f528519abb65575fbc2df6f799b3b44cc23d1f
AgusBrr <anbravoperez@gmail.com>
2022-10-24 15:13:27 -0300

    [FIX] KeycloakClient ahora valida si su Access Token expiró (y busca uno nuevo)

--- internal/auth/keycloak.go
+++ internal/auth/keycloak.go
@@ -3,7 +3,9 @@ package auth
 import (
 	"context"
 	"ctd-money-house/internal/domain"
+	"fmt"
 	"github.com/Nerzal/gocloak/v12"
+	"time"
 )
 
 type KeycloakClient interface {
@@ -18,36 +20,35 @@ type KeycloakClient interface {
 type JWT = gocloak.JWT
 
 type keycloakClient struct {
-	jwt          *gocloak.JWT
-	client       *gocloak.GoCloak
-	ctx          context.Context
-	realm        string
-	clientID     string
-	clientSecret string
+	jwt            *gocloak.JWT
+	client         *gocloak.GoCloak
+	ctx            context.Context
+	realm          string
+	clientID       string
+	clientSecret   string
+	tokenExpiresAt int64
 }
 
 // NewKeycloakClient instancia un KeycloakClient. Puede fallar si la petición de LoginClient a Keycloak falla.
 func NewKeycloakClient(url, clientID, clientSecret, realm string) (KeycloakClient, error) {
-	client := gocloak.NewClient(url)
-	ctx := context.Background()
-	token, err := client.LoginClient(ctx, clientID, clientSecret, realm)
-	if err != nil {
-		return &keycloakClient{}, err
-	}
-
-	return &keycloakClient{
-		jwt:          token,
-		client:       client,
-		clientSecret: clientSecret,
-		clientID:     clientID,
+	k := &keycloakClient{
+		client:       gocloak.NewClient(url),
+		ctx:          context.Background(),
 		realm:        realm,
-		ctx:          ctx,
-	}, nil
+		clientID:     clientID,
+		clientSecret: clientSecret,
+	}
+	err := k.loginClient()
+	if err != nil {
+		return nil, err
+	}
+	return k, nil
 }
 
 // GetUserByID solicita los datos del usuario con el userID dado.
 // El userID es el string GUID autogenerado por Keycloak. Es distinto al el domain.User.ID.
 func (k *keycloakClient) GetUserByID(userID string) (domain.User, error) {
+	k.getTokenIfExpired()
 	kcUser, err := k.client.GetUserByID(k.ctx, k.jwt.AccessToken, k.realm, userID)
 	if err != nil {
 		return domain.User{}, err
@@ -58,6 +59,7 @@ func (k *keycloakClient) GetUserByID(userID string) (domain.User, error) {
 // CreateUser registra un nuevo usuario en Keycloak y devuelve su userID.
 func (k *keycloakClient) CreateUser(user domain.User) (string, error) {
 	kcUser := fromDomainUser(user)
+	k.getTokenIfExpired()
 	return k.client.CreateUser(k.ctx, k.jwt.AccessToken, k.realm, kcUser)
 }
 
@@ -72,14 +74,39 @@ func (k *keycloakClient) LogoutUser(refreshToken string) error {
 }
 
 // RefreshToken solicita un JWT nuevo para el usuario. Keycloak genera un nuevo Access Token.
-// Por seguridad usa Refresh Token Rotation y genera un nuevo Refresh Token, invalidando el anterior.
+// Por seguridad se usa Refresh Token Rotation y genera un nuevo Refresh Token, invalidando el anterior.
 func (k *keycloakClient) RefreshToken(refreshToken string) (*JWT, error) {
 	return k.client.RefreshToken(k.ctx, refreshToken, k.clientID, k.clientSecret, k.realm)
 }
 
 // SetPassword establece una nueva contraseña para el usuario con el userID dado.
 func (k *keycloakClient) SetPassword(userID, password string) error {
+	k.getTokenIfExpired()
 	return k.client.SetPassword(k.ctx, k.jwt.AccessToken, userID, k.realm, password, false)
+}
+
+// loginClient obtiene un nuevo JWT para el keycloakClient, y calcula el nuevo tokenExpiresAt.
+func (k *keycloakClient) loginClient() error {
+	token, err := k.client.LoginClient(k.ctx, k.clientID, k.clientSecret, k.realm)
+	if err != nil {
+		return err
+	}
+	fmt.Printf("%+#v", token)
+	k.jwt = token
+	k.tokenExpiresAt = time.Now().Unix() + int64(token.ExpiresIn)
+	return nil
+}
+
+// getTokenIfExpired valida (antes de cada petición) que el Access Token del KeycloakClient no esté vencido.
+// Si el Access Token está vencido, realiza un Login del Client para obtener uno nuevo.
+func (k *keycloakClient) getTokenIfExpired() {
+	if k.tokenExpiresAt < time.Now().Unix() {
+		err := k.loginClient()
+		if err != nil {
+			// Si el login del cliente falla, entonces la operación fracasará por 401 Unauthorized.
+			return
+		}
+	}
 }
 
 func toDomainUser(kcUser *gocloak.User) domain.User {
